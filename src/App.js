ccdc8473449a828ff1bce3bfc75279f5ee2e9480
mondalnilavra13 <126654958+mondalnilavra13@users.noreply.github.com>
2023-04-10 17:52:16 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -37,3 +37,53 @@ function App() {
 }
 
 export default App;
+
+
+///
+/*
+import React, { useState } from 'react';
+
+function App() {
+  const [number, setNumber] = useState(0);
+
+  const generateTable = () => {
+    const table = [];
+    for (let i = 1; i <= 10; i++) {
+      table.push(<tr key={i}><td>{i}</td><td>{number * i}</td></tr>);
+    }
+    return table;
+  };
+
+  const updateNumber = () => {
+    const randomNum = Math.floor(Math.random() * 10) + 1;
+    setNumber(randomNum);
+  };
+
+  return (
+    <div>
+      <h1>Number Table Generator</h1>
+      <label htmlFor="numberInput">Number:</label>
+      <input
+        type="number"
+        id="numberInput"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={updateNumber}>Random Number</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Number</th>
+            <th>Result</th>
+          </tr>
+        </thead>
+        <tbody>
+          {generateTable()}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
+*/
